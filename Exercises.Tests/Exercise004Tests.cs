00e30480cf4e43d2b7ea2550936de17c59132ecd
Paz Sonagara <pazsonagara@Pazs-MacBook-Air.local>
2022-08-23 15:15:05 +0100

    Tests added to handle exception. More tests need to be added

--- Exercises.Tests/Exercise004Tests.cs
+++ Exercises.Tests/Exercise004Tests.cs
@@ -6,6 +6,8 @@ namespace Exercises.Tests
 {
     public class Exercise004Tests
     {
+        
+
         [Test]
         public void AddGigaSecond_Should_Return_Result_When_Only_Date_Is_Specified()
         {
@@ -28,6 +30,29 @@ namespace Exercises.Tests
             DateTime actualResult = Exercise004.AddGigaSecond(new DateTime(2021, 1, 24, 23, 59, 59, 0));
             DateTime expectedResult = new(2052, 10, 3, 1, 46, 39);
             actualResult.Should().Be(expectedResult);
+        }
+
+        [Test]
+        public void AddGigaSecond_Should_Throw_Exception_When_DateTime_Has_Incorrect_Range_Of_Value()
+        {
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => Exercise004.AddGigaSecond(new DateTime(2021, 1, 23, 24, 59, 59, 0)));
+            Assert.That(ex.Message, Is.EqualTo("Hour, Minute, and Second parameters describe an un-representable DateTime."));
+
+            ex = Assert.Throws<ArgumentOutOfRangeException>(() => Exercise004.AddGigaSecond(new DateTime(2021, 1, 23, 23, 60, 59, 0)));
+            Assert.That(ex.Message, Is.EqualTo("Hour, Minute, and Second parameters describe an un-representable DateTime."));
+
+            ex = Assert.Throws<ArgumentOutOfRangeException>(() => Exercise004.AddGigaSecond(new DateTime(2021, 1, 23, 23, 59, 60, 0)));
+            Assert.That(ex.Message, Is.EqualTo("Hour, Minute, and Second parameters describe an un-representable DateTime."));
+
+            ex = Assert.Throws<ArgumentOutOfRangeException>(() => Exercise004.AddGigaSecond(new DateTime(10000, 1, 23, 23, 59, 59, 0)));
+            Assert.That(ex.Message, Is.EqualTo("Year, Month, and Day parameters describe an un-representable DateTime."));
+
+            ex = Assert.Throws<ArgumentOutOfRangeException>(() => Exercise004.AddGigaSecond(new DateTime(2021, 13, 23, 23, 59, 59, 0)));
+            Assert.That(ex.Message, Is.EqualTo("Year, Month, and Day parameters describe an un-representable DateTime."));
+
+            ex = Assert.Throws<ArgumentOutOfRangeException>(() => Exercise004.AddGigaSecond(new DateTime(2021, 1, 32, 23, 59, 59, 0)));
+            Assert.That(ex.Message, Is.EqualTo("Year, Month, and Day parameters describe an un-representable DateTime."));
+
         }
     }
 }

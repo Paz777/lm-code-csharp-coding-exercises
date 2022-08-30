76f97e48a91eacb8a03dbb3df3801a5bc97b7d33
Paz Sonagara <pazsonagara@Pazs-MacBook-Air.local>
2022-08-30 15:30:57 +0100

    Changed use of a magic number to represent gigsecond

--- Exercises/Exercise004.cs
+++ Exercises/Exercise004.cs
@@ -3,7 +3,9 @@ namespace Exercises
 {
     public class Exercise004
     {
+        private static int gigaSecond = 1000000000;
+
         public static DateTime AddGigaSecond(DateTime dateTime)
-            => dateTime.AddSeconds(1000000000);
+            => dateTime.AddSeconds(gigaSecond);
     }
 }
